bbbee2313f540b9eed7f55659a76c2ef9ea3fab9
Wei-Ting Tu <bluesoul@ms17.hinet.net>
2019-05-02 19:58:27 +0800

    Support initial solution

--- initialsolution.cpp
+++ initialsolution.cpp
@@ -4,15 +4,16 @@
 
 static const double g_diesel_price = 26.3;
 static const double g_water_price  = 17;
+static const double g_M            = 1000;
 
 InitialSolution::InitialSolution(const TruckManager &t, const WaterStationManager &m) :
     _t(t),
     _m(m),
-    _cost()
+    _cost_matrix()
 {
 }
 
-void InitialSolution::_compute_cost(std::vector<std::vector<double> >& cost, double km_per_liter)
+void InitialSolution::_compute_cost_matrix(std::vector<std::vector<double> >& cost, double km_per_liter)
 {
     size_t size = _m.get_station_size();
 
@@ -25,20 +26,165 @@ void InitialSolution::_compute_cost(std::vector<std::vector<double> >& cost, dou
         {
             const WaterStation& target = _m.get_station(j);
             cost[i][j] = target.supply * g_water_price + ((double)_m.get_distance(i, j)) / 1000 / km_per_liter * g_diesel_price;
+//            if(i == 0 && j == 2)
+//            {
+//                printf("cost = %f = %f * %f + %d / 1000 / %f * %f\n",
+//                       cost[i][j],
+//            target.supply,  g_water_price, _m.get_distance(i, j),
+//                       km_per_liter, g_diesel_price
+//                       );
+//            }
         }
     }
 }
 
-void InitialSolution::_compute_cost()
+void InitialSolution::_compute_cost_matrix()
 {
     size_t truck_size = _t.get_truck_size();
-    _cost.clear();
-    _cost.resize(truck_size);
+    _cost_matrix.clear();
+    _cost_matrix.resize(truck_size);
 
     for(size_t i = 0; i < truck_size; ++i)
     {
         const Truck& truck = _t.get_truck(i);
-        _compute_cost(_cost.at(i), truck.km_per_liter);
+        _compute_cost_matrix(_cost_matrix.at(i), truck.km_per_liter);
+    }
+}
+
+double InitialSolution::_compute_solution_cost(size_t truck_idx, const std::vector<size_t>& stations) const
+{
+    double all_cost = 0.0;
+    size_t source_idx = 0;
+    for(size_t i = 0; i < stations.size(); ++i)
+    {
+        size_t target_idx = stations[i];
+        double cost = _cost_matrix.at(truck_idx).at(source_idx).at(target_idx);
+        printf("%zu truck %zu -> %zu cost %f\n", truck_idx, source_idx, target_idx, cost );
+        all_cost += cost;
+        source_idx = target_idx;
+    }
+
+    return all_cost;
+}
+
+double InitialSolution::_compute_all_solution_cost(const std::vector<std::vector<size_t> >& truck_stations) const
+{
+    double all_cost = 0.0;
+    for(size_t i = 0; i < truck_stations.size(); ++i)
+    {
+        double cost = _compute_solution_cost(i, truck_stations[i]);
+        printf("truck %zu cost %f\n", i, cost);
+        all_cost += cost;
+    }
+
+    return all_cost;
+}
+
+size_t InitialSolution::_get_min_distance(size_t source_idx, const std::set<size_t>& targets) const
+{
+    int min_distance = std::numeric_limits<int>::max();
+    size_t min_idx = std::numeric_limits<size_t>::max();
+
+    for(auto ite = targets.begin(); ite != targets.end(); ++ite)
+    {
+        size_t target_idx = *ite;
+        int distance = _m.get_distance(source_idx, target_idx);
+        if(distance < min_distance)
+        {
+            min_distance = distance;
+            min_idx = target_idx;
+        }
+    }
+
+    return min_idx;
+}
+
+std::vector<size_t> InitialSolution::_get_nearby_solution(const std::vector<size_t>& truck_stations) const
+{
+    std::vector<size_t> solution;
+    std::set<size_t> targets(truck_stations.begin(), truck_stations.end());
+
+    size_t source_idx = 0;
+    while(!targets.empty())
+    {
+        size_t target_idx = _get_min_distance(source_idx, targets);
+        solution.push_back(target_idx);
+        targets.erase(target_idx);
+        source_idx = target_idx;
+    }
+
+    return solution;
+}
+
+bool InitialSolution::_check_solution(const std::vector<size_t>& stations,
+                                      const std::vector<std::vector<size_t> >& truck_stations )
+{
+    size_t s = 0;
+    for(const std::vector<size_t>&v : truck_stations)
+    {
+        s += v.size();
+    }
+    if(s == stations.size())
+    {
+        printf("finished\n");
+        return true;
+    }
+    if(s < stations.size())
+    {
+        printf("not finished\n");
+        return false;
+    }
+    printf("error\n");
+    return true;
+}
+
+void InitialSolution::_add_missing_stations(const std::vector<size_t>& stations,
+                                      const std::vector<size_t>& trucks,
+                                      const std::vector<bool>& visited,
+                                      std::vector<std::vector<size_t> >& truck_stations )
+{
+    (void) stations;
+    (void) trucks;
+    std::vector<double> loads(3, 0.0);
+    for(size_t i = 0; i < truck_stations.size(); ++i)
+    {
+        const std::vector<size_t>& stations = truck_stations[i];
+        for(size_t j = 0; j < stations.size(); ++j)
+        {
+            const WaterStation& water_station = _m.get_station(stations[j]);
+            loads[i] += water_station.supply;
+        }
+    }
+
+    for(size_t i = 0; i < visited.size(); ++i)
+    {
+        if(visited[i])
+        {
+            continue;
+        }
+        double min_cost = std::numeric_limits<double>::max();
+        size_t min_idx = std::numeric_limits<size_t>::max();
+        printf("compute penalty for station %zu\n", i);
+        const WaterStation& water_station = _m.get_station(i);
+        for(size_t j = 0; j < loads.size(); ++j)
+        {
+            printf("compute penalty for truck %zu\n", j);
+            const Truck& truck = _t.get_truck(j);
+            double penalty = (loads[j] + water_station.supply - truck.load) * g_M;
+            printf("penalty = (%f + %f - %f ) * %f = %f\n", loads[j], water_station.supply, truck.load, g_M, penalty);
+
+            std::vector<size_t> solution = truck_stations[j];
+            solution.push_back(i);
+            solution = _get_nearby_solution(solution);
+
+            double cost = _compute_solution_cost(j, solution) + penalty;
+            if(cost < min_cost)
+            {
+                min_cost = cost;
+                min_idx = j;
+            }
+        }
+        truck_stations.at(min_idx).push_back(i);
     }
 }
 
@@ -63,6 +209,46 @@ bool InitialSolution::_get_max_distance(size_t source_idx, const std::vector<boo
     }
 
     return max_idx != std::numeric_limits<size_t>::max();
+}
+
+bool InitialSolution::_get_min_3_distance_within_load(size_t source_idx, double load, const std::vector<bool>& visited,
+                                                          size_t& min_idx) const
+{
+    // find min supply station within 3 min nearby stations
+    min_idx = std::numeric_limits<size_t>::max();
+    int min_distance = std::numeric_limits<int>::max();
+    size_t count = 0;
+    const std::vector<WaterStationDistance>& min_distances = _m.get_min_distances(source_idx);
+
+    for(size_t i = 0; i < min_distances.size(); ++i)
+    {
+        const WaterStationDistance& d = min_distances.at(i);
+        if(d.idx == source_idx)
+        {
+            continue;
+        }
+        if(visited.at(d.idx))
+        {
+            continue;
+        }
+        const WaterStation& water_station = _m.get_station(d.idx);
+        if(water_station.supply > load)
+        {
+            continue;
+        }
+        ++count;
+
+        if(min_distance > d.distance)
+        {
+            min_idx      = d.idx;
+            min_distance = d.distance;
+        }
+        if(count == 3)
+        {
+            break;
+        }
+    }
+    return min_idx != std::numeric_limits<size_t>::max();
 }
 
 bool InitialSolution::_get_min_3_distance_with_min_supply(size_t source_idx, const std::vector<bool>& visited,
@@ -122,7 +308,6 @@ double InitialSolution::_generate(const std::vector<size_t>& stations,
     }
     visited.at(0) = true;
 
-    double cost = 0.0;
     for(size_t truck_seq_idx = 0; truck_seq_idx < trucks.size(); ++truck_seq_idx)
     {
         size_t truck_idx = trucks.at(truck_seq_idx);
@@ -149,11 +334,10 @@ double InitialSolution::_generate(const std::vector<size_t>& stations,
                    load, water_station.supply);
             load -= water_station.supply;
             visited.at(target_idx) = true;
-            cost += _cost.at(truck_idx).at(source_idx).at(target_idx);
             truck_stations.at(truck_idx).push_back(target_idx);
 
             source_idx = target_idx;
-            if(!_get_min_3_distance_with_min_supply(source_idx, visited, target_idx ))
+            if(!_get_min_3_distance_within_load(source_idx, load, visited, target_idx ))
             {
                 printf("unable to get min distance station, next truck\n");
                 break;
@@ -162,13 +346,24 @@ double InitialSolution::_generate(const std::vector<size_t>& stations,
         }
     }
 
+    if(!_check_solution( stations, truck_stations ))
+    {
+        _add_missing_stations( stations, trucks, visited, truck_stations );
+    }
+
+    for(size_t i = 0; i < truck_stations.size(); ++i)
+    {
+        truck_stations[i] = _get_nearby_solution(truck_stations[i]);
+    }
+
+    double cost = _compute_all_solution_cost(truck_stations);
+
     return cost;
 }
 
 void InitialSolution::_generate(const std::vector<size_t>& stations)
 {
-    //std::vector<size_t> trucks = {0, 1, 2};
-    std::vector<size_t> trucks = {1, 2, 0};
+    std::vector<size_t> trucks = {0, 1, 2};
     std::vector<std::vector<size_t> > min_truck_stations(trucks.size(), std::vector<size_t>());
     double min_cost = std::numeric_limits<double>::max();
     do
@@ -187,7 +382,9 @@ void InitialSolution::_generate(const std::vector<size_t>& stations)
 
 void InitialSolution::generate()
 {
-    _compute_cost();
+    _compute_cost_matrix();
+    _generate(_m.get_schedule(1));
+    return;
     for(size_t i = 0; i < _m.get_schedule_size(); ++i)
     {
         _generate(_m.get_schedule(i));
--- initialsolution.h
+++ initialsolution.h
@@ -2,6 +2,7 @@
 #define INITIALSOLUTION_H
 
 #include <vector>
+#include <set>
 #include <crtdefs.h>
 
 class WaterStationManager;
@@ -13,17 +14,27 @@ public:
     InitialSolution(const TruckManager& t, const WaterStationManager& m);
     void generate();
 private:
+    double _compute_solution_cost(size_t truck_idx, const std::vector<size_t>& stations) const;
+    double _compute_all_solution_cost(const std::vector<std::vector<size_t> >& truck_stations) const;
+    size_t _get_min_distance(size_t source_idx, const std::set<size_t>& targets) const;
+    std::vector<size_t> _get_nearby_solution(const std::vector<size_t>& truck_stations) const;
+    void _add_missing_stations(const std::vector<size_t>& stations,
+                         const std::vector<size_t>& trucks,
+                         const std::vector<bool>& visited,
+                         std::vector<std::vector<size_t> >& truck_stations );
     bool _get_max_distance(size_t source_idx, const std::vector<bool>& visited, size_t &max_idx) const;
+    bool _get_min_3_distance_within_load(size_t source_idx, double load, const std::vector<bool>& visited, size_t& min_idx) const;
     bool _get_min_3_distance_with_min_supply(size_t source_idx, const std::vector<bool>& visited , size_t &min_idx) const;
+    bool _check_solution(const std::vector<size_t>& stations, const std::vector<std::vector<size_t> >& truck_stations );
     double _generate(const std::vector<size_t>& stations, const std::vector<size_t>& trucks,
                      std::vector<std::vector<size_t> > &truck_stations);
     void _generate(const std::vector<size_t>& stations);
-    void _compute_cost(std::vector<std::vector<double> > &cost, double km_per_liter);
-    void _compute_cost();
+    void _compute_cost_matrix(std::vector<std::vector<double> > &cost, double km_per_liter);
+    void _compute_cost_matrix();
 
     const TruckManager& _t;
     const WaterStationManager& _m;
-    std::vector<std::vector<std::vector<double> > > _cost;
+    std::vector<std::vector<std::vector<double> > > _cost_matrix;
 };
 
 #endif // INITIALSOLUTION_H
